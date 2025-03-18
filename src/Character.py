87dc10a7f46fed432b8582a05165f27beacf0203
MrSuricate <kevindu032270@gmail.com>
2025-03-18 10:26:30 +0100

    update de la classe Personnage.py

--- src/Character.py
+++ src/Character.py
@@ -2,15 +2,23 @@ from typing import Self
 
 class Character:
     def __init__(self):
-        self.hp = 100
-        self.attack = 5
-        self.est_mort = False
+        self.name: str = ""
+        self.hp: int = 100
+        self.damage: int = 5
+        self.critical_chance: int = 5
+        self.is_dead: bool = False
 
     def attack(self, enemy: Self):
-        if not self.est_mort:
-            enemy.received_damage(self.attack)
+        if not self.is_dead:
+            enemy.receive_damage(self.damage)
+            print(self.name + " attacks " + enemy.name + " for " + self.damage + " points of damage")
+        else:
+            print(self.name + " cannot attack because is dead")
 
-    def received_damage(self, damage: int):
+
+    def receive_damage(self, damage: int):
         self.hp -= damage
+        print(self.name + " received " + damage + " points of damage")
         if self.hp <= 0:
-            self.est_mort = True
+            self.is_dead = True
+            print(self.name + " is dead")
