25f5a9227a8adde2b926c859310e47837d98eed3
Antoine Cerdan <eosiswastaken@gmail.com>
2025-03-18 10:58:08 +0100

    add sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src import Character
-
+import time
 player1 = Character.Character("john")
 player2 = Character.Character("karen")
 
@@ -7,6 +7,7 @@ while not player1.is_dead and not player2.is_dead:
     player1.attack(player2)
     if not player2.is_dead:
         player2.attack(player1)
+    time.sleep(1)
 
 if player1.is_dead:
     print(player2.name + "won !")
