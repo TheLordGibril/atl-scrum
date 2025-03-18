5812b47037c5636681d0c1ab0744d6979975345d
MrSuricate <kevindu032270@gmail.com>
2025-03-18 10:26:30 +0100

    update de la classe Personnage.py

--- src/Character.py
+++ src/Character.py
@@ -1,17 +1,16 @@
+from typing import Self
+
 class Character:
     def __init__(self):
         self.hp = 100
         self.attack = 5
         self.est_mort = False
 
-    def get_hp(self):
-        return self.hp
-
-    def attack(self, enemy):
+    def attack(self, enemy: Self):
         if not self.est_mort:
             enemy.received_damage(self.attack)
 
-    def received_damage(self, damage):
+    def received_damage(self, damage: int):
         self.hp -= damage
         if self.hp <= 0:
             self.est_mort = True
