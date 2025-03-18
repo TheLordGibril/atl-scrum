63e86ce5582e994f815ade5cdb6c6bbafb36f161
Antoine Cerdan <eosiswastaken@gmail.com>
2025-03-18 10:55:59 +0100

    main loop and type fixes

--- src/Character.py
+++ src/Character.py
@@ -1,7 +1,7 @@
 from typing import Self
 
 class Character:
-    def __init__(self, name):
+    def __init__(self,name):
         self.name: str = name
         self.hp: int = 100
         self.damage: int = 5
@@ -11,14 +11,14 @@ class Character:
     def attack(self, enemy: Self):
         if not self.is_dead:
             enemy.receive_damage(self.damage)
-            print(self.name + " attacks " + enemy.name + " for " + self.damage + " points of damage")
+            print(self.name + " attacks " + enemy.name + " for " + str(self.damage) + " points of damage")
         else:
             print(self.name + " cannot attack because is dead")
 
 
     def receive_damage(self, damage: int):
         self.hp -= damage
-        print(self.name + " received " + damage + " points of damage")
+        print(self.name + " received " + str(damage) + " points of damage")
         if self.hp <= 0:
             self.is_dead = True
             print(self.name + " is dead")
